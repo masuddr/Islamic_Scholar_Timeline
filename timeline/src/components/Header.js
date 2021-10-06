7edec7a6a45d2e22a7d88fc5d3ac590c22a5d941
Masud Rasa <39189522+masuddr@users.noreply.github.com>
2021-10-06 17:26:34 +0200

    added search bar
    Co-Authored-By: Ahmeddzz <81853174+Ahmeddzz@users.noreply.github.com>

--- timeline/src/components/Header.js
+++ timeline/src/components/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
-// import SearchIcon from "@material-ui/icons/Search";
+import SearchIcon from "@material-ui/icons/Search";
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+
+
 import {
   AppBar,
   Toolbar,
@@ -9,6 +13,47 @@ import {
   Typography,
   makeStyles,
 } from "@material-ui/core";
+
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+  }));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  }));
 
 const useStyles = makeStyles((theme) => ({
   navlinks: {
@@ -44,6 +89,15 @@ function Header() {
         <Typography variant="h4" className={classes.logo}>
           Heroes of the Islam
         </Typography>
+        <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
         <div className={classes.navlinks}>
           <Link to="/" className={classes.link}>
             Home
